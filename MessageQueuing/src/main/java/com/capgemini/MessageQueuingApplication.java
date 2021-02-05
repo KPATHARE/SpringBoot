60ec2f99fc80fdbeb640d04dde571b93e84bb56e
KPATHARE <76953869+KPATHARE@users.noreply.github.com>
2021-02-05 17:35:04 +0530

    Add files via upload

---
+++ MessageQueuing/src/main/java/com/capgemini/MessageQueuingApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageQueuingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageQueuingApplication.class, args);
+	}
+
+}
