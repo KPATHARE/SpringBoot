ff7e76662d86f1c0832843f41a48d2cb5ac88ae3
KPATHARE <76953869+KPATHARE@users.noreply.github.com>
2021-02-05 17:15:23 +0530

    Add files via upload

---
+++ Exception/src/main/java/com/capgemini/ResTwithJpaApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTwithJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTwithJpaApplication.class, args);
+	}
+
+}
